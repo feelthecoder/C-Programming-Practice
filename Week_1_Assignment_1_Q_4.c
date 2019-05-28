21758a325f4a7a4a8f00903abb91bccc651f41fa
Vikas Kumar <44005233+feelthecoder@users.noreply.github.com>
2019-05-29 00:38:23 +0530

    Week 1 Assignment_1  Q 4

---
+++ Week_1_Assignment_1_Q_4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,m=0,i;
+	printf("Enter two numbers for multiplication:");
+scanf("%d%d",&a,&b);
+for(i=1;i<=b;i++)
+ m+=a;
+printf("Multiplication Result :%d",m);
+}
