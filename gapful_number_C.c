6bd089bfde0fb3d213ccc1e9c3fe8617a43ce95a
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-06-15 20:53:41 +0530

    Update gapful_number_C.c

--- gapful_number_C.c
+++ gapful_number_C.c
@@ -2,14 +2,14 @@
 int gapful(int number);
 int main(void)
 {
-	int num_,return_val;
-    printf("Enter the number you want to check??\n");
+	int num_;
+        printf("Enter the number you want to check??\n");
 	scanf("%d",&num_);
-	return_val=gapful(num_);//function calling to find gapful number
-	if(return_val==0)
-	printf("\nGiven number is not gapful\n");
+	//function calling to find gapful number
+	if(gapful(num_)) 
+	    printf("\nGiven number is not gapful\n");
 	else
-	printf("Given number is gapful\n");
+	    printf("Given number is gapful\n");
 	return 0;	
 }
 
@@ -19,19 +19,16 @@ int gapful(int number)
 	int count=0,last_,first_,another_,new_formed;
 	last_=number%10;
 	another_=number;
-	while(number!=0){
+	while(number){
 		first_=number%10;
-		number=number/10;
+		number /=10;
 		count++;
 	}
-	new_formed=first_*10+last_;
-	if(count==3)
+	if(count>=3)
 	{
+           new_formed= first_*10 + last_;
 	if(another_%new_formed==0)
-	return 1;
-	else 
-	return 0;
-}
-else 
+	    return 1;
+        }
 return 0;
 }
